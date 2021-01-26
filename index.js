8002899a8b5722cfad104b90809cc144f6f77f97
Aleksandr Lee <lexxlee@rambler.ru>
2021-01-26 16:52:58 +0300

    вывод результата

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ const debounce = (fn, ms) => {
 };
 function change() {
   getRepositories(input.value).then((data) => {
-	  console.log(data)
-});
+    console.log(data);
+  });
 }
 
 changeDebounce = debounce(change, 500);
 input.addEventListener("input", changeDebounce);
 
 async function getRepositories(login) {
-	if (login === '') return
+  if (login === "") return;
   let response = await fetch(
     `https://api.github.com/search/repositories?q=${login}`
   );
@@ -32,62 +32,75 @@ async function getRepositories(login) {
 }
 
 function searchResults(res) {
-   div.innerHTML = "";
+  div.innerHTML = "";
   res.forEach((element) => {
-    div.innerHTML += `
-		 <div class="result">${element.name}</div>`;
+	  div.insertAdjacentHTML('beforeend', `<div class="result">${element.name}</div>`)
+   //  div.innerHTML += `
+	// 	 <div class="result">${element.name}</div>`;
   });
-
-  div.addEventListener("click", (e) => {
-	 input.value = "";
-	 div.innerHTML = "";
-	 console.log(e.target)
+  div.addEventListener("click", function (e) {
+    input.value = "";
+    div.innerHTML = "";
+    res.forEach((element) => {
+      divCard.innerHTML += `
+			         <p class="card__item card__name">Name: ${element.name}</p>
+						<p class="card__item card__owner">Owner: ${element.owner.login}</p>
+						<p class="card__item card__stars">Stars: ${element.stargazers_count}</p>
+			`;
+	 });
   });
 }
+// function activ(elem) {
+// 	console.log(elem)
+// }
+// div.addEventListener("click", function (e)  {
+// 	// console.log( input.value);
+// 	input.value = "";
+// 	div.innerHTML = "";
+// 	// activ(e)
+//    // console.log(e.target);
+//    // for (let key of res) {
+//   	//  console.log(key)
 
+//    // }
+//  });
+// let html = 	`<p class="card__item card__name">Name: ${data[0].name}</p>
+// 				<p class="card__item card__owner">Owner: ${data[0].owner.login}</p>
+// 				<p class="card__item card__stars">Stars: ${data[0].stargazers_count}</p>
+// 	`;
+// data.forEach((element) => {
 
+// divCard.insertAdjacentHTML("beforeend", html)
+//   divCard.innerHTML = `
+// 	<p class="card__item card__name">Name: ${element.name}</p>
+// 				<p class="card__item card__owner">Owner: ${element.owner.login}</p>
+// 				<p class="card__item card__stars">Stars: ${element.stargazers_count}</p>
+// 	`;
+//  let html = `
+// 			<p class="card__item card__name">${element.name}</p>
+// 			<p class="card__item card__owner"></p>
+// 			<p class="card__item card__stars"></p>
+// 		`
+// 	 console.log(html)
+// });
 
-	// let html = 	`<p class="card__item card__name">Name: ${data[0].name}</p>
-		// 				<p class="card__item card__owner">Owner: ${data[0].owner.login}</p>
-		// 				<p class="card__item card__stars">Stars: ${data[0].stargazers_count}</p>
-		// 	`;
-      // data.forEach((element) => {
-		
-			// divCard.insertAdjacentHTML("beforeend", html)
-      //   divCard.innerHTML = `
-		// 	<p class="card__item card__name">Name: ${element.name}</p>
-		// 				<p class="card__item card__owner">Owner: ${element.owner.login}</p>
-		// 				<p class="card__item card__stars">Stars: ${element.stargazers_count}</p>
-		// 	`;
-        //  let html = `
-        // 			<p class="card__item card__name">${element.name}</p>
-        // 			<p class="card__item card__owner"></p>
-        // 			<p class="card__item card__stars"></p>
-        // 		`
-        // 	 console.log(html)
-		// });
-		
+// div.addEventListener("click", (e) => {
+// 	console.log(e)
+//   input.value = "";
+//   div.innerHTML = "";
 
-		// div.addEventListener("click", (e) => {
-		// 	console.log(e)
-		//   input.value = "";
-		//   div.innerHTML = "";
-	  
-		// });
+// });
 
+// 	div.addEventListener("click", (e) => {
+// 		input.value = "";
+// 		div.innerHTML = "";
 
+// 		data.forEach((element) => {
+// 			let html = 	`<p class="card__item card__name">Name: ${element.name}</p>
+// 			<p class="card__item card__owner">Owner: ${element.owner.login}</p>
+// 			<p class="card__item card__stars">Stars: ${element.stargazers_count}</p>
+// `;
+// 			divCard.insertAdjacentHTML("beforeend", html)
 
-	// 	div.addEventListener("click", (e) => {
-	// 		input.value = "";
-	// 		div.innerHTML = "";
-		
-	// 		data.forEach((element) => {
-	// 			let html = 	`<p class="card__item card__name">Name: ${element.name}</p>
-	// 			<p class="card__item card__owner">Owner: ${element.owner.login}</p>
-	// 			<p class="card__item card__stars">Stars: ${element.stargazers_count}</p>
-	// `;
-	// 			divCard.insertAdjacentHTML("beforeend", html)
-		  
-	
-	// 	 });
-	//   });
+// 	 });
+//   });
